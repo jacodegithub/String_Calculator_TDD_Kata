399a29af027579398d1b09a4e60a4cfd1f0b5137
Jack <dudeobama246@gmail.com>
2023-05-28 16:17:31 +0530

    9TH Commit

--- String_Calculator/Test/com/string_calculator/test/Calculator.java
+++ String_Calculator/Test/com/string_calculator/test/Calculator.java
@@ -1,85 +1,107 @@
 package com.string_calculator.test;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.ArrayList;
 
 public class Calculator {
+	private static final String MORE = "more";
+	private static final String ONE = "one";
+	private static final String MESSAGE = "Negatives are not allowed";
+	private static final int SINGLE_LENGTH = 1;
+	private static final int ERROR = -1;
+	private static final int EMPTY = 0;
+	private List<Integer> list = new ArrayList<>();
 	
 	public int add(String string) {
 		boolean comma = string.contains(",");
 		boolean newLineTag = string.contains("\n");
 		boolean doubleSlash = string.contains("//");
 		
-		List<Integer> list = new ArrayList<>();
 		
-		if(string.isEmpty()) return 0;
 		
-		if(string.length() == 1) {
-			if(Character.isDigit(string.charAt(0))) return 1;
-			else return -1;
+		if(string.isEmpty()) return EMPTY;
+		
+		if(string.length() == SINGLE_LENGTH) {
+			if(Character.isDigit(string.charAt(0))) return SINGLE_LENGTH;
+			else return ERROR;
 		}
 		
 		int sum = 0;
+		String check = checkForNegative(string);
 		if(doubleSlash) {
-			if(newLineTag) {
-				string = string.replace("\n", ";");
-			}
-			string = string.replace("//", "").replace(";", "");
-			for(char s : string.toCharArray()) {
-				if(Character.isLetter(s)) {
-					return -1;
+			if(check == ONE) throw new NumberFormatException(MESSAGE);
+			else if(check == MORE) throw new NumberFormatException("Negatives are not allowed :"+list);
+			else {
+				string = string.replaceAll("[^-0-9]", "");
+				if(string.isEmpty()) return ERROR;
+				for(char s : string.toCharArray()) {
+					if(Character.isLetter(s)) {
+						return ERROR;
+					}
+					sum += Integer.parseInt(String.valueOf(s));
 				}
-				if(Integer.parseInt(String.valueOf(s)) < 0) {
-					list.add(Integer.parseInt(String.valueOf(s)));
-				}
-				sum += Integer.parseInt(String.valueOf(s));
 			}
-			if(list.size() == 1) throw new NumberFormatException("Negatives are not allowed");
-			else if(list.size() > 1) throw new NumberFormatException("Negatives are not allowed :"+list);
 		}
 		else if(comma && newLineTag) {
-			string = string.replace("\n", "").replace(",", "");
-			for(char s : string.toCharArray()) {
-				if(Character.isLetter(s)) {
-					return -1;
+			if(check == ONE) throw new NumberFormatException(MESSAGE);
+			else if(check == MORE) throw new NumberFormatException("Negatives are not allowed :"+list);
+			else {
+				string = string.replaceAll("[^-0-9]", "");
+				if(string.isEmpty()) return ERROR;
+				for(char s : string.toCharArray()) {
+					if(Character.isLetter(s)) {
+						return ERROR;
+					}
+					sum += Integer.parseInt(String.valueOf(s));
 				}
-				if(Integer.parseInt(String.valueOf(s)) < 0) {
-					list.add(Integer.parseInt(String.valueOf(s)));
-				}
-				sum += Integer.parseInt(String.valueOf(s));
 			}
-			if(list.size() == 1) throw new NumberFormatException("Negatives are not allowed");
-			else if(list.size() > 1) throw new NumberFormatException("Negatives are not allowed :"+list);
 		}
 		else if(newLineTag) {
-			string = string.replace("\n", "");
-			for(char s : string.toCharArray()) {
-				if(Character.isLetter(s)) {
-					return -1;
+			if(check == ONE) throw new NumberFormatException(MESSAGE);
+			else if(check == MORE) throw new NumberFormatException("Negatives are not allowed :"+list);
+			else {
+				string = string.replaceAll("[^-0-9]", "");
+				if(string.isEmpty()) return ERROR;
+				for(char s : string.toCharArray()) {
+					if(Character.isLetter(s)) {
+						return ERROR;
+					}
+					sum += Integer.parseInt(String.valueOf(s));
 				}
-				if(Integer.parseInt(String.valueOf(s)) < 0) {
-					list.add(Integer.parseInt(String.valueOf(s)));
-				}
-				sum += Integer.parseInt(String.valueOf(s));
 			}
-			if(list.size() == 1) throw new NumberFormatException("Negatives are not allowed");
-			else if(list.size() > 1) throw new NumberFormatException("Negatives are not allowed :"+list);
 		}
 		else if(comma) {
-			String[] str = string.split(",");
-			for(String s : str) {
-				if(Character.isLetter(s.charAt(0))) {
-					return -1;
+			if(check == ONE) throw new NumberFormatException(MESSAGE);
+			else if(check == MORE) throw new NumberFormatException("Negatives are not allowed :"+list);
+			else {
+				string = string.replaceAll("[^-0-9]", "");
+				if(string.isEmpty()) return ERROR;
+				for(char s : string.toCharArray()) {
+					if(Character.isLetter(s)) {
+						return ERROR;
+					}
+					sum += Integer.parseInt(String.valueOf(s));
 				}
-				if(Integer.parseInt(String.valueOf(s)) < 0) {
-					list.add(Integer.parseInt(String.valueOf(s)));
-				}
-				sum += Integer.parseInt(String.valueOf(s));
 			}
-			if(list.size() == 1) throw new NumberFormatException("Negatives are not allowed");
-			else if(list.size() > 1) throw new NumberFormatException("Negatives are not allowed :"+list);
 		}
 		return sum;
+	}
+	
+	public String checkForNegative(String string) {
+		
+		Pattern pattern = Pattern.compile("-\\d+");
+        Matcher matcher = pattern.matcher(string);
+        
+        while(matcher.find()) {
+        	int num = Integer.parseInt(matcher.group());
+        	list.add(num);
+        }
+        
+        if(list.size() == 1) return ONE;
+        else if(list.size() > 1) return  MORE;
+        else return "NA";
 	}
 	
 	public List<Integer> getNegativeNumbers(NumberFormatException exception) {
--- String_Calculator/Test/com/string_calculator/test/StringCalculator.java
+++ String_Calculator/Test/com/string_calculator/test/StringCalculator.java
@@ -9,57 +9,64 @@ import org.junit.jupiter.api.Test;
 
 class StringCalculator {
 
+	private static final String MESSAGE = "Negatives are not allowed";
+	private static final int NUMBER_SUM_IF_NEWLINETAG = 6;
+	private static final int NUMBER_SUM_IF_DBLSTASH_NEWLINETAG = 6;
+	private static final int NUMBER_SUM_IF_COMMA = 6;
+	private static final int NUMBER_SUM_IF_NEWLINETAG_COMMA = 6;
+	private static final int INVALID_SINGLE_DIGIT = -1;
+	private static final int SINGLE_DIGIT = 1;
+	private static final int ZERO = 0;
+
 	@Test
 	public void checkForEmptyString() {
 		Calculator obj = new Calculator();
-		int result = obj.add("");
-		assertEquals(0, result);
+		long result = obj.add("");
+		assertEquals(ZERO, result);
 	}
 	
 	@Test
 	public void checkForTheSingleDigitString() {
 		Calculator obj = new Calculator();
-		int result = obj.add("1");
-		assertEquals(1, result);
+		long result = obj.add("1");
+		assertEquals(SINGLE_DIGIT, result);
 	}
 	
 	@Test
-	public void checkForValidSingleLengthString() {
+	public void checkForInValidSingleLengthString() {
 		Calculator obj = new Calculator();
-		int result = obj.add("a");
-		assertEquals(-1, result);
+		long result = obj.add("a");
+		assertEquals(INVALID_SINGLE_DIGIT, result);
 	}
 	
 	@Test
 	public void checkForTheSumOfNumbersInStringWithComma() {
 		Calculator obj = new Calculator();
 		long result = obj.add("1,2,3");
-		assertEquals(6, result);
+		assertEquals(NUMBER_SUM_IF_COMMA, result);
 	}
 	
 	@Test
 	public void checkForInvalidStringWithComma() {
 		Calculator obj = new Calculator();
-		int result = obj.add("a,b");
-		assertEquals(-1, result);
+		long result = obj.add("a,b");
+		assertEquals(INVALID_SINGLE_DIGIT, result);
 	}
 	
 	@Test
 	public void checkForTheSumOfNumbersInStringNewLineTag() {
 		Calculator obj = new Calculator();
 		long result = obj.add("1\n2,3");
-		assertEquals(6, result);
-		result = obj.add("2\n4\n1");
-		assertEquals(7, result);
+		assertEquals(NUMBER_SUM_IF_NEWLINETAG_COMMA, result);
+		result = obj.add("1\n2\n3");
+		assertEquals(NUMBER_SUM_IF_NEWLINETAG, result);
 	}
 	
 	@Test
-	public void checkForCoubleSlashInTheStringWithSemiColon() {
+	public void checkForDoubleSlashInTheStringWithSemiColon() {
 		Calculator obj = new Calculator();
-		int result = obj.add("//;1;2;3");
-		assertEquals(6, result);
-		result = obj.add("//;1\n2;3");
-		assertEquals(6, result);
+		long result = obj.add("//;1\n2;3");
+		assertEquals(NUMBER_SUM_IF_DBLSTASH_NEWLINETAG, result);
 	}
 	
 	@Test
@@ -70,7 +77,7 @@ class StringCalculator {
 				() -> {
 					obj.add("1,-2,4");
 				});
-		assertEquals("Negatives are not allowed", exception.getMessage());
+		assertEquals(MESSAGE, exception.getMessage());
 	}
 	
 	@Test 
